7710b4800be3b6de24ac81dd9fa54332aa5434ee
majoortizg <157551392+majoortizg@users.noreply.github.com>
2025-04-04 20:11:37 -0600

    Add files via upload

---
+++ backend/controllers/tareasControllers.js
@@ -0,0 +1,7 @@
+const getTareas = (req,res)=>{
+    res.send('Obtener Tareas')
+}
+
+module.exports = {
+    getTareas
+}
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/tareas', require('./routes/tareasRoutes'))
+
+app.listen(port, ()=>console.log(`Servidor iniciado en el puerto ${port}`))
+
